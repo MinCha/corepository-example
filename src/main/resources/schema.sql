a05d00a91dc9c43e12ef4eee80ef4a7f0b787a4b
Min Cha <minslovey@gmail.com>
2012-07-25 16:49:17 +0900

    Applied the last CoRepository version

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,3 @@
-CREATE TABLE visitation (name varchar(20), age varchar(50), visitationCount int);
-INSERT INTO visitation VALUES ('min', '33', 0);
-INSERT INTO visitation VALUES ('cha', '18', 5);
+CREATE TABLE visitation (name varchar(20), visitationCount int);
+INSERT INTO visitation VALUES ('min', 0);
+INSERT INTO visitation VALUES ('cha', 5);
--- src/main/webapp/WEB-INF/example/home.jsp
+++ src/main/webapp/WEB-INF/example/home.jsp
@@ -1,4 +1,4 @@
 <%@ page contentType="text/html; charset=UTF-8"%> 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-CoRepository : ${visitation.count}
-Database : ${databaseVisitation.value}
+CoRepository : ${corepo.count}
+Database : ${db.count}
